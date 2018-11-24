57e9f4974958eedb42b7621f5b6de56a13741c26
Germiona <violetta.ashirova@gmail.com>
2018-11-24 20:24:05 +0300

    legend

--- js/stat.js
+++ js/stat.js
@@ -3,12 +3,13 @@
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var BOTTOM_GAP = 40;
-var FONT_GAP = BOTTOM_GAP / 2;
+var FONT_GAP = 5;
 var CLOUD_X = 130;
-var CLOUD_Y = CLOUD_HEIGHT - BOTTOM_GAP; // 240;
+var CLOUD_Y = CLOUD_HEIGHT - FONT_GAP * 5; // 240;
 var BAR_WIDTH = 40;
-var BAR_GAP = BAR_WIDTH + BAR_WIDTH / 2;
+var BAR_GAP = 50;
 var BAR_HEIGHT = -150;
+var FONT_SIZE = 16;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -28,7 +29,10 @@ var getMaxElement = function (arr) {
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.3)');
   renderCloud(ctx, 100, 10, '#fff');
-
+  ctx.font = '16px PT Mono';
+  ctx.fillStyle = '#444';
+  ctx.fillText('Ура вы победили!', CLOUD_X, BOTTOM_GAP);
+  ctx.fillText('Список результатов:', CLOUD_X, BOTTOM_GAP + FONT_SIZE);
 
   var maxTime = getMaxElement(times);
 
@@ -38,7 +42,9 @@ window.renderStatistics = function (ctx, players, times) {
     } else {
       ctx.fillStyle = 'hsl(240,' + Math.floor(Math.random() * 100) + '%,' + Math.floor(Math.random() * 100) + '%' + ')';
     }
-    ctx.fillText(players[i], CLOUD_X + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - FONT_GAP);
     ctx.fillRect(CLOUD_X + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y, BAR_WIDTH, (BAR_HEIGHT * times[i] / maxTime));
+    ctx.fillStyle = '#444';
+    ctx.fillText(Math.floor(times[i]), CLOUD_X + (BAR_WIDTH + BAR_GAP) * i, (BAR_HEIGHT * times[i] / maxTime) + CLOUD_Y - FONT_GAP);
+    ctx.fillText(players[i], CLOUD_X + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - FONT_GAP);
   }
 };
